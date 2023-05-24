e2cd8caed3e8b8e2c5490b31f985039e39736cde
JustinePend <justinetulip@gmail.com>
2023-05-24 16:48:51 -0700

    fixed name

--- main-scene.js
+++ main-scene.js
@@ -7,7 +7,7 @@ import {Scene_To_Texture_Demo} from "./examples/scene-to-texture-demo.js"
 import {Surfaces_Demo} from "./examples/surfaces-demo.js"
 import {Text_Demo} from "./examples/text-demo.js"
 import {Transforms_Sandbox} from "./examples/transforms-sandbox.js"
-import {Assignment4} from "./assignment4.js";
+import {TromboneHero} from "./trombonehero.js";
 
 // Pull these names into this module's scope for convenience:
 const {
@@ -34,14 +34,14 @@ Object.assign(defs,
     {Surfaces_Demo},
     {Text_Demo},
     {Transforms_Sandbox},
-    {Assignment4}
+    {TromboneHero}
 );
 
 // ******************** End extra step
 
 // (Can define Main_Scene's class here)
 
-const Main_Scene = Assignment4;
+const Main_Scene = TromboneHero;
 const Additional_Scenes = [];
 
 export {Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs}
